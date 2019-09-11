45b6ce05670d11f5a7890690f8b52ff945500b2e
Samuel Cho <choss@wfu.edu>
2019-09-11 01:11:00 -0400

    Initial commit

---
+++ MaxSort.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// function declarations
+void maxSort(vector<int>& list);
+void swap(int& a, int& b);
+void print(const vector<int>& list);
+
+int main() {
+
+    vector<int> v(5);  // vector to be sorted
+    // initialize v
+    /* your code here */
+
+
+    // print the unsorted v
+    /* your code here */
+
+
+    // sort v
+    /* your code here */
+
+
+    // print the sorted v
+    /* your code here */
+
+
+    return 0;
+}
+
+/* the function definitions */
+
+//Max sort function
+void maxSort(vector<int>& list){
+    for(int i=list.size()-1;i>=0;i--){
+        /*complete the code*/
+
+        for(int j=0;j<=i;j++){
+            /*complete the code*/
+
+        }
+        /*complete the code*/
+    }
+}
+
+// swap function
+void swap(int& a, int&b){
+    /*your code here*/
+}
+
+// print function
+void print(const vector<int>& list){
+    /*your code here*/
+}
